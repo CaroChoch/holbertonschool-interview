70fbd0417f7f197696f9db19ac14e76bfbf835d9
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-01-12 13:40:08 +0000

    function in C that inserts a number into a sorted singly linked list

---
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -0,0 +1,55 @@
+#include "lists.h"
+
+/**
+ * add_node - Adds a number to a sorted linked list.
+ *            of a list_t list.
+ * @head: A pointer to the head of the list_t list.
+ * @number: The number to be added to the list_t list.
+ *
+ * Return: If the function fails - NULL.
+ *         Otherwise - the address of the new node.
+ */
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new;
+    listint_t *current;
+
+    if (head == NULL)
+    	return (NULL);
+
+    new = malloc(sizeof(listint_t));
+
+    if (new == NULL)
+        return (NULL);
+
+    new->n = number;
+    new->next = NULL;
+
+    current = *head;
+
+    if (current == NULL)
+    {
+        *head = new;
+        return (new);
+    }
+
+    if (current->n > number)
+    {
+        new->next = current;
+        *head = new;
+        return (new);
+    }
+
+    while (current->next != NULL)
+    {
+        if (current->next->n > number)
+            break;
+        current = current->next;
+    }
+
+    new->next = current->next;
+    current->next = new;
+
+    return (new);
+}
---
+++ insert_in_sorted_linked_list/README.md
@@ -0,0 +1,8 @@
+# Insert in sorted linked list
+
+Write a function in C that inserts a number into a sorted singly linked list.
+
+- Prototype: `listint_t *insert_node(listint_t **head, int number)``;
+- Return: the address of the new node, or `NULL` if it failed
+
+The programs and functions will be compiled with `gcc 4.8.4` using the flags `-Wall -Werror -Wextra and -pedantic`
