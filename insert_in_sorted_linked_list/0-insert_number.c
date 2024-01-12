f20bd41f5e6880b06a768beeb38e1995dd259311
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-01-12 15:57:55 +0000

    return in the first code version

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -15,7 +15,8 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *new;
     listint_t *current;
 
-	current = *head;
+    if (head == NULL)
+    	return (NULL);
 
     new = malloc(sizeof(listint_t));
 
@@ -23,20 +24,32 @@ listint_t *insert_node(listint_t **head, int number)
         return (NULL);
 
     new->n = number;
+    new->next = NULL;
 
-    while (current != NULL && current->next != NULL && number > current->next->n )
-		current = current->next;
-    
-    if ((current == NULL) || (current->n > number))
-	{
-        new->next = *head;
-		*head = new;
-	}
-	else
-	{
-		new->next = current->next;
-		current->next = new;
-	}
+    current = *head;
+
+    if (current == NULL)
+    {
+        *head = new;
+        return (new);
+    }
+
+    if (current->n > number)
+    {
+        new->next = current;
+        *head = new;
+        return (new);
+    }
+
+    while (current->next != NULL)
+    {
+        if (current->next->n > number)
+            break;
+        current = current->next;
+    }
+
+    new->next = current->next;
+    current->next = new;
 
     return (new);
 }
