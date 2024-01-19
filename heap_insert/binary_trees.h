be112769c6d43c819167ee9882121809dacfdaa8
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-01-19 17:31:11 +0000

    update header file

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -15,18 +15,23 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
+
 
 typedef struct binary_tree_s heap_t;
-typedef struct binary_tree_s binary_tree_t;
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-heap_t *heap_insert(heap_t **root, int value);
+
+/* Binary_tree_print.c */
 void binary_tree_print(const binary_tree_t *);
+/* 0-binary_tree_node.c */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+/* 1-heap_insert.c */
+heap_t *heap_insert(heap_t **root, int value);
+
 
 #endif /* BINARY_TREES_H */
