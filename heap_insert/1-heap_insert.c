fe73f421add9823709ac5b77ce656351b6393c36
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-01-19 20:35:10 +0000

    fix space at the start line

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,6 +1,5 @@
 #include "binary_trees.h"
 
-
 /**
  * binary_tree_size - measures the size of a binary tree
  *
@@ -58,36 +57,36 @@ heap_t *find_parent(heap_t *root, size_t index, size_t size)
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-    heap_t *new_node, *parent, *tmp;
-    size_t tree_size;
-    int temp_value;
+	heap_t *new_node, *parent, *tmp;
+	size_t tree_size;
+	int temp_value;
 
-    if (root == NULL)
-        return (NULL);
-    new_node = binary_tree_node(NULL, value);
-    if (new_node == NULL)
-        return (NULL);
-    if ((*root) == NULL)
-    {
-        *root = new_node;
-        return (new_node);
-    }
-    tree_size = binary_tree_size(*root);
-    parent = find_parent(*root, 0, (tree_size - 1) / 2);
-    if (!parent->left)
-        parent->left = new_node;
-    else
-        parent->right = new_node;
-    new_node->parent = parent;
+	if (root == NULL)
+		return (NULL);
+	new_node = binary_tree_node(NULL, value);
+	if (new_node == NULL)
+		return (NULL);
+	if ((*root) == NULL)
+	{
+		*root = new_node;
+		return (new_node);
+	}
+	tree_size = binary_tree_size(*root);
+	parent = find_parent(*root, 0, (tree_size - 1) / 2);
+	if (!parent->left)
+		parent->left = new_node;
+	else
+		parent->right = new_node;
+	new_node->parent = parent;
 
-    tmp = new_node;
-    while (tmp->parent && tmp->n > tmp->parent->n)
-    {
-        temp_value = tmp->n;
-        tmp->n = tmp->parent->n;
-        tmp->parent->n = temp_value;
-        tmp = tmp->parent;
-    }
+	tmp = new_node;
+	while (tmp->parent && tmp->n > tmp->parent->n)
+	{
+		temp_value = tmp->n;
+		tmp->n = tmp->parent->n;
+		tmp->parent->n = temp_value;
+		tmp = tmp->parent;
+	}
 
-    return (tmp);
+	return (tmp);
 }
