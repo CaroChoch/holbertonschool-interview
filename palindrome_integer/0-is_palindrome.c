e12d79faefec6711ec4d3f4efd3c17bc16fa0652
CaroChoch <fleur-a-fleur59@hotmail.fr>
2024-02-27 09:08:47 +0000

    Palindrome unsigned integer

---
+++ palindrome_integer/0-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "palindrome.h"
+
+/**
+ * is_palindrome - Checks if a given unsigned integer is a palindrome or not
+ * @n: The unsigned integer to be checked.
+ *
+ * Return: If the integer is a palindrome - 1.
+ *         Otherwise - 0
+ */
+
+int is_palindrome(unsigned long n)
+{
+    char str[100]; /* Define a character array to store the number */
+    int length = 0;
+
+    /* Convert the number to a string manually */
+    while (n > 0) {
+        str[length++] = n % 10 + '0';
+        n /= 10;
+    }
+    str[length] = '\0'; /* Terminate the string with a null character */
+
+    /* Check if the string is a palindrome */
+    for (int i = 0; i < length / 2; i++) {
+        if (str[i] != str[length - i - 1]) {
+            return 0; /* If characters do not match, it's not a palindrome */
+        }
+    }
+    return 1; /* If all characters match, it's a palindrome */
+}
---
+++ palindrome_integer/palindrome.h
@@ -0,0 +1,10 @@
+#ifndef _PALINDROME_H_
+#define _PALINDROME_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int is_palindrome(unsigned long n);
+
+#endif
