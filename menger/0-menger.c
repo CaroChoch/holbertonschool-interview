0b7c63db822a94b8fbb5e1f99c21860b1e78be32
root <fleur-a-fleur59@hotmail.fr>
2024-03-27 15:50:26 +0000

    solve betty

--- menger/0-menger.c
+++ menger/0-menger.c
@@ -4,6 +4,7 @@
 * @level: Level of the Menger Sponge to draw
 * Return: void
 */
+
 void menger(int level)
 {
 	int i, j, size, x, y;
@@ -14,27 +15,27 @@ void menger(int level)
 	size = pow(3, level);
 
     /* Loop to iterate over each row of the Menger Sponge */
-    for (i = 0; i < size; i++)
-    {
-        /* Loop to iterate over each column of the Menger Sponge */
-        for (j = 0; j < size; j++)
-        {
-            x = i; /* Assign i to x */
-            y = j; /* Assign j to y */
-            c = '#'; /* Assign # to c */
-            /* Loop to determine if the current cell should be empty */
+	for (i = 0; i < size; i++)
+	{
+		/* Loop to iterate over each column of the Menger Sponge */
+		for (j = 0; j < size; j++)
+		{
+			x = i; /* Assign i to x */
+			y = j; /* Assign j to y */
+			c = '#'; /* Assign # to c */
+			/* Loop to determine if the current cell should be empty */
 			while (x > 0)
-            {
-                /* If conditions for empty cell are met, set c to ' ' */
-                if (x % 3 == 1 && y % 3 == 1)
-                    c = ' ';
-                x = x / 3; /* Reduce x coordinate */
-                y = y / 3; /* Reduce y coordinate */
-            }
-            /* Print the character */
-            printf("%c", c);
-        }
-        /* Print a new line */
-        printf("\n");
-    }
+			{
+				/* If conditions for empty cell are met, set c to ' ' */
+				if (x % 3 == 1 && y % 3 == 1)
+					c = ' ';
+					x = x / 3; /* Reduce x coordinate */
+					y = y / 3; /* Reduce y coordinate */
+			}
+			/* Print the character */
+			printf("%c", c);
+		}
+		/* Print a new line */
+		printf("\n");
+	}
 }
