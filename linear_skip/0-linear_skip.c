b1dec035cbc9b4099564e0c43fe2d01bc542e69f
root <fleur-a-fleur59@hotmail.fr>
2024-04-19 14:01:23 +0000

    betty coding style

--- linear_skip/0-linear_skip.c
+++ linear_skip/0-linear_skip.c
@@ -20,26 +20,21 @@ skiplist_t *linear_skip(skiplist_t *list, int value)
 		node = express;
 		express = express->express;
 	}
-
 	if (express != NULL)
 		printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-
-    else
+	else
 	{
 		last = node;
 		while (last->next != NULL)
 			last = last->next;
 	}
-
 	if (express != NULL)
 		printf("Value found between indexes [%lu] and [%lu]\n",
-    node->index, express->index);
-
-    else
+	node->index, express->index);
+	else
 		printf("Value found between indexes [%lu] and [%lu]\n",
-    node->index, last->index);
-
-    while (node != NULL && node->n < value)
+	node->index, last->index);
+	while (node != NULL && node->n < value)
 	{
 		printf("Value checked at index [%lu] = [%d]\n", node->index, node->n);
 		node = node->next;
