0636f6ce67ad22a2c9436be86aa5309ec5749df2
root <fleur-a-fleur59@hotmail.fr>
2024-07-19 16:45:11 +0000

    Double Circular Linked List

---
+++ double_circular_linked_list/0-add_node.c
@@ -0,0 +1,72 @@
+#include "list.h"
+
+/**
+ * add_node_end - adds a new node at the end of a double circular linked list
+ * @list: double pointer to the list
+ * @str: string to be added to the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_end(List **list, char *str)
+{
+    List *new_node = malloc(sizeof(List));
+
+    if (new_node == NULL)
+        return (NULL);
+    new_node->str = strdup(str);
+    if (!new_node->str)
+    {
+        free(new_node);
+        return (NULL);
+    }
+    if (*list == NULL)
+    {
+        new_node->prev = new_node;
+        new_node->next = new_node;
+        *list = new_node;
+    }
+    else
+    {
+        new_node->prev = (*list)->prev;
+        new_node->next = *list;
+        (*list)->prev->next = new_node;
+        (*list)->prev = new_node;
+    }
+    return (new_node);
+}
+
+/**
+ * add_node_begin - adds a new node at the beginning of a double circular linked list
+ * @list: double pointer to the list
+ * @str: string to be added to the new node
+ *
+ * Return: Address of the new node, or NULL on failure
+ */
+List *add_node_begin(List **list, char *str)
+{
+    List *new_node = malloc(sizeof(List));
+
+    if (new_node == NULL)
+        return (NULL);
+    new_node->str = strdup(str);
+    if (new_node->str == NULL)
+    {
+        free(new_node);
+        return (NULL);
+    }
+    if (*list == NULL)
+    {
+        new_node->prev = new_node;
+        new_node->next = new_node;
+        *list = new_node;
+    }
+    else
+    {
+        new_node->prev = (*list)->prev;
+        new_node->next = *list;
+        (*list)->prev->next = new_node;
+        (*list)->prev = new_node;
+        *list = new_node;
+    }
+    return (new_node);
+}
