22c753f882df93ab824b052c2520057487a679f5
root <fleur-a-fleur59@hotmail.fr>
2024-09-01 19:38:34 +0000

    add a condition

--- regex/regex.c
+++ regex/regex.c
@@ -11,6 +11,9 @@ int regex_match(char const *str, char const *pattern)
 {
     if (*str == '\0' && *pattern == '\0')
         return (1);
+	if (*pattern == '\0')
+		return (0);
+
     if (*str == *pattern)
         return (regex_match(str + 1, pattern + 1));
     if (*pattern == '.')
