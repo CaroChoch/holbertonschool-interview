128b6b7f6780f20a2f74345a6881ab382dfe1133
root <fleur-a-fleur59@hotmail.fr>
2024-09-01 19:50:25 +0000

    add a loop

--- regex/regex.c
+++ regex/regex.c
@@ -1,6 +1,5 @@
 #include "regex.h"
 
-
 /**
  * regex_match - compares two strings
  * @str: string to scan
@@ -11,28 +10,23 @@ int regex_match(char const *str, char const *pattern)
 {
     if (*str == '\0' && *pattern == '\0')
         return (1);
-	if (*pattern == '\0')
-		return (0);
 
-    if (*str == *pattern)
-        return (regex_match(str + 1, pattern + 1));
+    if (*pattern == '\0')
+        return (0);
 
-    if (*pattern == '.')
+    if (*(pattern + 1) != '*')
     {
-        if (*(pattern + 1) == '*')
-            return (regex_match(str, pattern + 2));
-        if (regex_match(str + 1, pattern + 1))
-            return (1);
-        return (regex_match(str + 1, pattern));
+        if (*str == *pattern || (*pattern == '.' && *str != '\0'))
+            return (regex_match(str + 1, pattern + 1));
+        else
+            return (0);
     }
 
-    if (*pattern == '*')
+    for (; *str == *pattern || (*pattern == '.' && *str != '\0'); str++)
     {
-        if (*(pattern + 1) == '*')
-            return (regex_match(str, pattern + 1));
-        if (regex_match(str, pattern + 1))
+        if (regex_match(str, pattern + 2))
             return (1);
-        return (regex_match(str + 1, pattern));
     }
-    return (0);
+
+    return (regex_match(str, pattern + 2));
 }
