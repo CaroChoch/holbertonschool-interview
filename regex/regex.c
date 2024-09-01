2351a9d73656f8ded4e945b1fb38e5c7eb4018d6
root <fleur-a-fleur59@hotmail.fr>
2024-09-01 19:28:39 +0000

    Simple RegEx

---
+++ regex/regex.c
@@ -0,0 +1,33 @@
+#include "regex.h"
+
+
+/**
+ * regex_match - compares two strings
+ * @str: string to scan
+ * @pattern: regular expression
+ * Return: 1 if the strings can be considered identical, otherwise return 0
+ */
+int regex_match(char const *str, char const *pattern)
+{
+    if (*str == '\0' && *pattern == '\0')
+        return (1);
+    if (*str == *pattern)
+        return (regex_match(str + 1, pattern + 1));
+    if (*pattern == '.')
+    {
+        if (*(pattern + 1) == '*')
+            return (regex_match(str, pattern + 2));
+        if (regex_match(str + 1, pattern + 1))
+            return (1);
+        return (regex_match(str + 1, pattern));
+    }
+    if (*pattern == '*')
+    {
+        if (*(pattern + 1) == '*')
+            return (regex_match(str, pattern + 1));
+        if (regex_match(str, pattern + 1))
+            return (1);
+        return (regex_match(str + 1, pattern));
+    }
+    return (0);
+}
