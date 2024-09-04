ed6b592795b049813ccfcc9934b3d62fd91a343d
root <fleur-a-fleur59@hotmail.fr>
2024-09-04 17:03:43 +0000

    solve betty coding style

--- substring/substring.c
+++ substring/substring.c
@@ -14,19 +14,19 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 	int len, word_len, i, *indices, *count;
 
 	if (!s || !words || !n || nb_words == 0)
-		return NULL;
+		return (NULL);
 
 	len = strlen(s);
 	word_len = strlen(words[0]);
 	indices = init_indices(len);
 	if (!indices)
-		return NULL;
+		return (NULL);
 
 	count = init_count(nb_words);
 	if (!count)
 	{
 		free(indices);
-		return NULL;
+		return (NULL);
 	}
 
 	for (i = *n = 0; i <= len - nb_words * word_len; i++)
@@ -36,7 +36,7 @@ int *find_substring(char const *s, char const **words, int nb_words, int *n)
 	}
 
 	free(count);
-	return indices;
+	return (indices);
 }
 
 /**
@@ -56,7 +56,7 @@ int *init_indices(int len)
  */
 int *init_count(int nb_words)
 {
-	return calloc(nb_words, sizeof(int));
+	return (calloc(nb_words, sizeof(int)));
 }
 
 /**
@@ -69,7 +69,12 @@ int *init_count(int nb_words)
  * @count: an array to store the number of times each word appears
  * Return: 1 if all words are present, otherwise 0
  */
-int check_words(const char *s, const char **words, int i, int nb_words, int word_len, int *count)
+int check_words(const char *s,
+				const char **words,
+				int i,
+				int nb_words,
+				int word_len,
+				int *count)
 {
 	int j, k;
 
@@ -84,10 +89,10 @@ int check_words(const char *s, const char **words, int i, int nb_words, int word
 				break;
 			}
 		}
-		if (k == nb_words) // No matching word found
-			return 0;
+		if (k == nb_words)
+			return (0);
 	}
-	return 1;
+	return (1);
 }
 
 /**
@@ -103,7 +108,7 @@ int add_index_if_match(int *indices, int *n, int i, int match)
 	if (match)
 	{
 		indices[*n] = i;
-		return *n + 1;
+		return (*n + 1);
 }
-	return *n;
+	return (*n);
 }
